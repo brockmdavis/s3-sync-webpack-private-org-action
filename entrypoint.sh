8c00447a17a4e95cc2ae288a9e176508ffe20ed0
Brock Daivs <brock.davis@turner.com>
2020-04-19 22:17:08 -0400

    added npm ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,16 +60,17 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-echo "Change directory to Source"
-cd $SOURCE_DIR
-
 echo "Install webpack"
 npm install webpack webpack-cli npm-cli-login
 
 echo "Set private org scope"
-
 npm-cli-login -u ${USERNAME} -p ${CI_CD_TOKEN} -e ${EMAIL} -r https://npm.pkg.github.com -s @${ORG_NAME}
 
+echo "Install dependencies"
+npm ci
+
+echo "Change directory to Source"
+cd $SOURCE_DIR
 
 echo "Run npx"
 npx webpack --mode development
